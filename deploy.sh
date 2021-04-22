0ec7049382777bd46d9bf85f713be2f5b50c1ac9
Rui Rua <ruirua@MacBook-Air-de-Rui.local>
2021-04-22 18:07:30 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
 
+
 rm -rf dist/*
 python -m incremental.update manafa --patch
+git add manafa/_version.py
+git commit -m "bump _version"
 python setup.py sdist
 python -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -6,10 +6,8 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 
-
 setup(
     name='manafa',
-    version="0.3.61",
     description='E-MANAFA: Energy Monitor and ANAlyzer For Android',
     long_description=long_description,
     long_description_content_type='text/markdown',
