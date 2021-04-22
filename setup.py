d531ea10ed5fd1309abdb886bd0f4d620d9d2c43
Rui Rua <ruirua@MacBook-Air-de-Rui.local>
2021-04-22 08:28:10 +0100

    travis update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='manafa',
-    version='0.3.33',
+    version='0.3.34',
     description='E-MANAFA: Energy Monitor and ANAlyzer For Android',
     long_description=long_description,
     long_description_content_type='text/markdown',
