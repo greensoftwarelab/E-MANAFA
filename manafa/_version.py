1237a7c9a602ed87884e60e7743a320c0a7450be
Rui Rua <ruirua@MacBook-Air-de-Rui.local>
2021-04-22 09:35:54 +0100

    version file

---
+++ manafa/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides manafa version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update manafa` to change this file.
+
+from incremental import Version
+
+__version__ = Version("manafa", 0, 3, 5)
+__all__ = ["__version__"]
