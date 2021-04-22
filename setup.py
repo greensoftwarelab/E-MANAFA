92d3c5f573a38c46d56cbb743827d7302ffcf9be
Rui Rua <ruirua@MacBook-Air-de-Rui.local>
2021-04-22 09:27:37 +0100

    auto versioning

--- setup.py
+++ setup.py
@@ -1,33 +1,39 @@
 import setuptools
 from setuptools import setup
+from manafa._version import __version__
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
+
+
 setup(
     name='manafa',
-    version='0.3.34',
+    version=__version__,
     description='E-MANAFA: Energy Monitor and ANAlyzer For Android',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Rui Rua',
-    author_email='rui.rrua@gmail.com',
+    author_email='rui.rrua@gmailgit.com',
     url='https://github.com/RRua/e-manafa',
     license='MIT',
     packages=setuptools.find_packages(),
+    use_incremental=True,
     install_requires=[
         'pytz==2021.1',
         'termcolor==1.1.0',
         'python-textops3==3.1.0',
         'python-dateutil==2.8.1',
         "setuptools>=42",
-        "wheel"
+        "wheel",
+        "incremental"
+
     ],
+    setup_requires=['incremental'],
     include_package_data=True,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Science/Research',
-        'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.8',
     ],
