3286e18a8dfa6f81e2d8e2d43df15a51c23b5cd8
Raphael Oliveira <raphinha_31@hotmail.com>
2021-05-13 16:51:48 +0100

    bump _version

--- manafa/_version.py
+++ manafa/_version.py
@@ -7,5 +7,5 @@ Provides manafa version information.
 
 from incremental import Version
 
-__version__ = Version('manafa', 0, 3, 67)
+__version__ = Version('manafa', 0, 3, 68)
 __all__ = ["__version__"]
