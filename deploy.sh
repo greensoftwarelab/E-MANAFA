b480ade56f29f16a59751a222f16064c5f02127b
Rui Rua <ruirua@Air-de-Rui.lan>
2021-06-07 11:13:24 +0100

    bug fix: missing manafa dir prefix in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 rm -rf dist/*
 #sed -i -e 's/DEV=True/DEV=False/g' "manafa/utils/Utils.py"
 python -m incremental.update manafa --patch
-echo "" > results/batterystats/__init__.py
-echo "" > results/hunter/__init__.py
-echo "" > results/perfetto/__init__.py
-git add manafa/_version.py manafa/utils/Utils.py results/*/__init__.py
+echo "" > manafa/results/batterystats/__init__.py
+echo "" > manafa/results/hunter/__init__.py
+echo "" > manafa/results/perfetto/__init__.py
+git add manafa/_version.py manafa/utils/Utils.py manafa/results/*/__init__.py
 git commit -m "bump _version"
 python setup.py sdist
 python -m twine upload dist/*
