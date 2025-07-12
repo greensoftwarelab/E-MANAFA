3bfd26693ced26e674d1529e68adf604b51fac56
Rui Rua <rui.rrua@gmail.com>
2025-07-12 09:31:57 +0100

    bump _version

--- manafa/_version.py
+++ manafa/_version.py
@@ -7,5 +7,5 @@ Provides manafa version information.
 
 from incremental import Version
 
-__version__ = Version("manafa", 0, 3, 133)
+__version__ = Version("manafa", 0, 3, 134)
 __all__ = ["__version__"]
